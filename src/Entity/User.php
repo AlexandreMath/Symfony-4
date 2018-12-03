33daf7ddda59586cf8273d7efc0c9a1171bbf4b9
AlexandreMath <alemathi@gmail.com>
2018-12-03 12:57:25 +0100

    feat: ajout de l'authentification

---
+++ src/Entity/User.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User implements UserInterface, \Serializable
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+    
+    //FROM Symfony/UserInterface
+
+     /**
+     * Returns the roles granted to the user.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        return ['ROLE_ADMIN'];
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return NULL;
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     * @return Null
+     */
+    public function eraseCredentials()
+    {}
+
+    /**
+     * Une chaine qui représente un objet
+     * @return string
+     */
+    public function serialize()
+    {
+        return serialize([
+            $this->id,
+            $this->username,
+            $this->password
+        ]);
+    }
+    
+    /**
+     * Construit l'objet
+     * @return void
+     */
+    public function unserialize($unserialize)
+    {
+        list($this->id,
+        $this->username,
+        $this->password) = unserialize($unserialize, ['allowed_classes' => false]);
+    }
+    
+   
+}
